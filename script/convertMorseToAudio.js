819c781817dcae7139b7de202fb3a895fce90c07
CleverAxel <90392657+CleverAxel@users.noreply.github.com>
2021-10-19 04:24:21 +0200

    Add files via upload

--- script/convertMorseToAudio.js
+++ script/convertMorseToAudio.js
@@ -1,6 +1,8 @@
 let dot = new Audio("../sound/dot.ogg");
 let dash = new Audio("../sound/dash.ogg");
 
+let lumiere = document.getElementById("backgroundMorse");
+
 const DOT_TIMER = 375;
 const DASH_TIMER = 750;
 const SPACE_TIMER = 200;
@@ -38,16 +40,26 @@ export default class ConvertMorseToAudio{
     }
 
     playDot(){
+        lumiere.style.backgroundColor = "black";
         dot.play();
         this.indice++;
+
+        setTimeout(() => {
+            lumiere.style.backgroundColor = "bisque";
+        }, DOT_TIMER-100);
         setTimeout(() => {
             this.convertToSound();
         }, DOT_TIMER);
     }
 
     playDash(){
+        lumiere.style.backgroundColor = "black";
         dash.play();
         this.indice++;
+
+        setTimeout(() => {
+            lumiere.style.backgroundColor = "bisque";
+        }, DASH_TIMER-100);
         setTimeout(() => {
             this.convertToSound();
         }, DASH_TIMER);
